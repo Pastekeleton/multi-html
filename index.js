f2fb9cafb7438517c0e7ede28106f3a699330c04
David <david.melo@lc.cuny.edu>
2023-02-07 18:12:09 -0500

    Finished

---
+++ index.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+
+var fname = prompt("Please enter your first name:");
+var lname = prompt("Please enter your last name:");
+
+console.log("Your name is", fname, lname);
